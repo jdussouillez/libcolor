d32b6a3e7c3200d93ae897f35ab219faf7af8fe4
jdussouillez <junior.dussouillez@gmail.com>
2014-02-15 11:17:36 +0100

    getcolor() optimization

--- src/color.c
+++ src/color.c
@@ -5,21 +5,12 @@
 
 #include "color.h"
 
-static char* getstrcolor(color_t c) {
+static char* getcolor(color_t c, int bgcolor) {
   int size = 6 * sizeof(char);
   char* str = malloc(size);
   memset(str, 0, size);
   if (c != NORMAL)
-    snprintf(str, size, "\e[3%dm]", (int)c);
-  return str;
-}
-
-static char* getstrbgcolor(color_t bgc) {
-  int size = 6 * sizeof(char);
-  char* str = malloc(size);
-  memset(str, 0, size);
-  if (bgc != NORMAL)
-    snprintf(str, size, "\e[4%dm]", (int)bgc);
+    snprintf(str, size, (bgcolor ? "\e[4%dm" : "\e[3%dm"), (int)c);
   return str;
 }
 
@@ -28,8 +19,8 @@ int cprintf(color_t fontcolor, color_t bgcolor, int bold, const char* format, ..
   char *strcolor, *strbgcolor;
   int nbcharprinted;
   // Set (= print) colors. If the color is NORMAL, an empty string is printed.
-  strcolor = getstrcolor(fontcolor);
-  strbgcolor = getstrbgcolor(bgcolor);
+  strcolor = getcolor(fontcolor, 0); // Get font color
+  strbgcolor = getcolor(bgcolor, 1); // Get background color
   printf("%s%s", strcolor, strbgcolor);
   free(strcolor);
   free(strbgcolor);
